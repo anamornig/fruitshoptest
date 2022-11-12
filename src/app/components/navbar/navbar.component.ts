eced24f4ced07690c46a0adaf4f2dd860614a246
anamornig <anamoruja@gmail.com>
2022-11-12 19:38:16 +0100

    add navbar with api

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AllDataService } from 'src/app/services/all-data.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  categoriesList: any;
+  constructor(private allDataService: AllDataService) { }
 
   ngOnInit(): void {
+    this.allDataService.getData().subscribe((data) =>{
+      this.categoriesList= data.categories;
+    })
   }
 
 }
